74cfe4ae3d2043466dc477be9e77845703d446eb
Alexander Jara <luisalexanderjara95@gmail.com>
2023-04-19 21:13:20 -0500

    corrigiendo Contacto namespace

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace appfunko.Models
+namespace Amyra.Models
 {
     [Table("t_contacto")]
     public class Contacto
