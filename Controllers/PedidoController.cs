6f07364284bc34da5a0836f436fbf92b8124d000
AlexanderJara95 <63384238+AlexanderJara95@users.noreply.github.com>
2023-05-31 09:50:19 -0500

    Corrigiendo PedidoController.cs

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
 using Microsoft.AspNetCore.Identity;
 
 using Amyra.Data;
@@ -13,71 +14,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Amyra.Controllers
 {
-    
-    public class PagoController : Controller
+    public class PedidoController : Controller
     {
-        private readonly ILogger<PagoController> _logger;
+        private readonly ILogger<PedidoController> _logger;
+
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ApplicationDbContext _context;
 
-        public PagoController(ILogger<PagoController> logger, UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        public PedidoController(ILogger<PedidoController> logger, UserManager<IdentityUser> userManager, ApplicationDbContext context)
         {
             _logger = logger;
             _userManager = userManager;
             _context = context;
         }
 
-        public IActionResult Create(Decimal monto)
+        public  async Task<IActionResult>  Index()
         {
-
-            Pago pago = new Pago();
-            pago.UserID = _userManager.GetUserName(User);
-            pago.MontoTotal = monto;
-            return View(pago);
-        }
-
-        [HttpPost]
-        public IActionResult Pagar(Pago pago)
-        {
-            pago.PaymentDate = DateTime.UtcNow;
-            _context.Add(pago);
+             var pedidos = from o in _context.DataPedido select o;
+             pedidos = pedidos.Where(s => s.Status.Contains("Pendientes"));
             
-            var itemsProforma = from o in _context.DataProformas select o;
-            itemsProforma = itemsProforma.
-                Include(p => p.Producto).
-                Where(s => s.UserID.Equals(pago.UserID) && s.Status.Equals("PENDIENTE"));
-
-            Pedido pedido = new Pedido();
-            pedido.UserID = pago.UserID;
-            pedido.Total = pago.MontoTotal;
-            pedido.pago = pago;
-            pedido.Status = "PENDIENTE";
-            _context.Add(pedido);
-
-            List<DetallePedido> itemsPedido = new List<DetallePedido>();
-            foreach(var item in itemsProforma.ToList()){
-                DetallePedido detallePedido = new DetallePedido();
-                detallePedido.pedido=pedido;
-                detallePedido.Precio = item.Precio;
-                detallePedido.Producto = item.Producto;
-                detallePedido.Cantidad = item.Cantidad;
-                itemsPedido.Add(detallePedido);
-            }
-
-
-            _context.AddRange(itemsPedido);
-
-            foreach (Proforma p in itemsProforma.ToList())
-            {
-                p.Status="PROCESADO";
-            }
-
-            _context.UpdateRange(itemsProforma);
-
-            _context.SaveChanges();
-
-            ViewData["Message"] = "El pago se ha registrado y su pedido nro "+ pedido.ID +" esta en camino";
-            return View("Create");
+            return View(await pedidos.ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
