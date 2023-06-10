e94825e95c1b64d90d8ebd06fc7469cc6e40c555
Alexander Jara <luisalexanderjara95@gmail.com>
2023-06-09 19:54:41 -0500

    cambios antes de p4

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 
@@ -12,11 +13,12 @@ namespace Amyra.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column("id")]
-        public int Id { get; set; }
+        public int Id { get; set; }  
+        [Display(Name = "Nombre del producto")]   
         public string? Name { get; set; }
-
+        [Display(Name = "Descripción")]   
         public string? Descripcion { get; set; }
-
+        [Display(Name = "Precio")]   
         public Decimal Precio { get; set; }
 
         public Decimal PorcentajeDescuento { get; set; }
